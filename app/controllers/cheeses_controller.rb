c7397e71ec4d9442b433b4b2918138b5239c9b96
m-njora <mmuthekijora@gmail.com>
2023-03-14 20:50:46 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,11 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
+  def show
+    #find a cheese by id
+    cheese = Cheese.find(params[:id])
+    #return a json object
+    render json: cheese
+  end
 
 end
